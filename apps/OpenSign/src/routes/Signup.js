78822bcb9fd231dab7b637f1df4b40dbf1668b95
prafull-opensignlabs <prafull.navkar@nxglabs.com>
2023-11-16 16:20:21 +0530

    fix: tenant is not create in sign up flow

--- apps/OpenSign/src/routes/Signup.js
+++ apps/OpenSign/src/routes/Signup.js
@@ -52,50 +52,27 @@ const Signup = (props) => {
       res
         .then(async (r) => {
           if (r) {
-            let roleurl = `${parseBaseUrl}functions/AddUserToRole`;
-            const headers = {
-              "Content-Type": "application/json",
-              "X-Parse-Application-Id": parseAppId,
-              sessionToken:
-                r.getSessionToken() || localStorage.getItem("accesstoken")
-            };
-            let body = {
-              appName: props.appInfo.appname,
-              roleName: props.appInfo.defaultRole,
-              userId: r.id
-            };
-            await axios.post(roleurl, body, { headers: headers });
-
             let roleData = props.appInfo.settings;
             if (roleData && roleData.length > 0) {
-              roleData.forEach((rld) => {
-                if (rld.role === props.appInfo.defaultRole) {
-                  const extCls = Parse.Object.extend(rld.extended_class);
-                  const newObj = new extCls();
-                  newObj.set("UserId", {
-                    __type: "Pointer",
-                    className: "_User",
-                    objectId: r.id
-                  });
-                  newObj.set("UserRole", props.appInfo.defaultRole);
-                  newObj.set("Email", email);
-                  newObj.set("Name", name);
-                  newObj.set("Phone", phone);
-                  newObj.set("Company", company);
-                  newObj.set("JobTitle", jobTitle);
-                  newObj
-                    .save()
-                    .then((ex) => {
-                      if (ex) {
-                        handleNavigation(r.getSessionToken());
-                      }
-                    })
-                    .catch((err) => {
-                      alert(err.message);
-                      setState({ loading: false });
-                    });
+              const params = {
+                userDetails: {
+                  jobTitle: jobTitle,
+                  company: company,
+                  name: name,
+                  email: email,
+                  phone: phone,
+                  role: props.appInfo.defaultRole
                 }
-              });
+              };
+              try {
+                const usersignup = await Parse.Cloud.run("usersignup", params);
+                if (usersignup) {
+                  handleNavigation(r.getSessionToken());
+                }
+              } catch (err) {
+                alert(err.message);
+                setState({ loading: false });
+              }
             }
           }
         })
@@ -208,12 +185,6 @@ const Signup = (props) => {
                       );
                       localStorage.setItem("_user_role", _role);
 
-                      if (element.enableCart) {
-                        localStorage.setItem("EnableCart", element.enableCart);
-                        props.setEnableCart(element.enableCart);
-                      } else {
-                        localStorage.removeItem("EnableCart");
-                      }
                       // Get TenentID from Extendend Class
                       localStorage.setItem(
                         "extended_class",
